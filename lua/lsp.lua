c8d4c2f1bbc8eb52e23c9cf594c218c12562b173
William <wpkelso+github@posteo.net>
2026-07-31 23:00:31 -0400

    Fix lsp servers not loading if nvim isn't launched from the servers dir

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,8 +1,10 @@
 vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled())
 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 
+local cpath = vim.fs.normalize("$HOME/.config/nvim/lua/server/")
+
 function lang(item)
-    c = dofile("server/" .. item .. ".lua")
+    c = dofile(vim.fs.joinpath(cpath, item) .. ".lua")
     vim.lsp.config(c["name"], c["config"])
     vim.lsp.enable(c["name"])
 end
